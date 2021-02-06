5cca104c292dfc23be279b36f9f561df0adcf042
Varun Sharma <coolvarunsharma117@gmail.com>
2021-02-07 00:38:54 +0530

    c1

---
+++ build/precache-manifest.f7fd1d59005af1b7fea67b5a299e4ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747e3c75da6f31a5043f07e3a73501fc",
+    "url": "/Template_1/index.html"
+  },
+  {
+    "revision": "188ef48d701d5155b3da",
+    "url": "/Template_1/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "4503ce833e42471a34dc",
+    "url": "/Template_1/static/js/2.e3d68ef1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Template_1/static/js/2.e3d68ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188ef48d701d5155b3da",
+    "url": "/Template_1/static/js/main.34e8b8da.chunk.js"
+  },
+  {
+    "revision": "6e5440395af5c1f08374",
+    "url": "/Template_1/static/js/runtime-main.8504f68f.js"
+  },
+  {
+    "revision": "d1460e1a28f938146e943284c9f2c813",
+    "url": "/Template_1/static/media/resume.d1460e1a.pdf"
+  }
+]);
